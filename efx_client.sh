9aff86e2cf62b6a09926a3450cc5882db93dcde2
UtsavBalar1231 <utsavbalar1231@gmail.com>
2023-10-12 18:03:18 +0530

    efx_client: fix quotings for variables
    Change-Id: Idd459a027f659e72a3accc4bb278ccde48c3ff67
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- efx_client.sh
+++ efx_client.sh
@@ -5,11 +5,11 @@ HOST_IP="192.168.29.12"
 SSH_ID="$HOST_NAME@$HOST_IP"
 PROJECT_NAME="$1"
 
-scp $PROJECT_NAME.xml *.v $SSH_ID:/tmp/$PROJECT_NAME
-ssh $SSH_ID /home/$HOST_NAME/.local/bin/efx_serv.sh $PROJECT_NAME
-scp $SSH_ID:/tmp/$PROJECT_NAME/$PROJECT_NAME.tar.gz ./
+scp ./*.xml ./*.v "$SSH_ID":/tmp/"$PROJECT_NAME"
+ssh "$SSH_ID" /home/$HOST_NAME/.local/bin/efx_serv.sh $PROJECT_NAME
+scp "$SSH_ID":/tmp/$PROJECT_NAME/"$PROJECT_NAME".tar.gz ./
 if [ $? -ne 0 ]; then
-    echo "failed"
-    exit 1
+	echo "failed"
+	exit 1
 fi
 tar -xzf $PROJECT_NAME.tar.gz
