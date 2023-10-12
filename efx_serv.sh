6beb861b6e84a9af654f7165498d4d93996f36de
UtsavBalar1231 <utsavbalar1231@gmail.com>
2023-10-12 17:51:25 +0530

    efx_serv: use the default script for efx_run
    the hack of calling multiple scripts manually seems not to
    be very fruitful as the default script is working just
    fine.
    
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>
    Change-Id: I7e4fc2377554f8b578470c8bf3f42846e8abeb40
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- efx_serv.sh
+++ efx_serv.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Paths to efinity on galactos
+# Kept here for redundancy and to make this script slightly more
+# self-contained
 export EFINITY_HOME=$HOME/hdd/shreeyash/efinity/2023.1/
 export EFXPT_HOME=$EFINITY_HOME/pt
 export EFXPGM_HOME=$EFINITY_HOME/pgm
@@ -9,10 +12,9 @@ export PATH=$PYTHON_PATH:$EFINITY_HOME/scripts:$PATH
 TARGET_DIR="$1"
 PROJECT_NAME="$1"
 OPTION_FULL_EXPORT=0
-MY_NAME="$(basename $0)"
-PATH_TO_SCRIPTS=$EFINITY_HOME/scripts
+MY_NAME="$(basename "$0")"
 
-if [ $# < 1 ]; then
+if [ $# -lt 1 ]; then
     echo "$MY_NAME: Too few arguments"
     echo "USAGE: $MY_NAME <project_name>"
     echo "NOTE: do not give .xml extension to project name"
@@ -24,25 +26,12 @@ fi
 
 cd /tmp/"$TARGET_DIR" || exit 1
 echo "$MY_NAME: Starting efinity process"
-$PATH_TO_SCRIPTS/efx_run.py "$PROJECT_NAME"
-if $PATH_TO_SCRIPTS/efx_run_map.py --project_xml "$PROJECT_NAME".xml $PROJECT_NAME |
-	tee -a /tmp/"$PROJECT_NAME"/outflow/"$PROJECT_NAME".log; then
+if efx_run.py "$PROJECT_NAME".xml; then
 	echo "$MY_NAME: Completed efinity process"
 else
-	echo "$MY_NAME: mapping failed"
+	echo "$MY_NAME: efinity failed"
 	exit 1
 fi
-
-if $PATH_TO_SCRIPTS/efx_run_pnr.py "$PROJECT_NAME" --prj --family Trion --device T120F324 \
-	--timing_model C4 --sim --output_dir outflow \
-	--opt work_dir=work_pnr seed=1 placer_effort_level=2 max_threads=-1 |
-	tee -a /tmp/"$PROJECT_NAME"/outflow/"$PROJECT_NAME".log; then
-	echo "$MY_NAME: Completed pnr process"
-else
-	echo "$MY_NAME: pnr failed"
-	exit 1
-fi
-
 echo "$MY_NAME: Creating tarball"
 if [ $OPTION_FULL_EXPORT = 0 ]; then
 	tar -cvzf "$PROJECT_NAME".tar.gz outflow
