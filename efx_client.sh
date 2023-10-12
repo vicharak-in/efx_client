e3e128f543c03321a3f6d7e200988eb4a55ea5e9
Shreeyash Pandey <shreeyash335@gmail.com>
2023-10-12 18:30:17 +0530

    efx_client: change abs path
    Signed-off-by: Shreeyash Pandey <shreeyash335@gmail.com>

--- efx_client.sh
+++ efx_client.sh
@@ -6,7 +6,7 @@ SSH_ID="$HOST_NAME@$HOST_IP"
 PROJECT_NAME="$1"
 
 scp ./*.xml ./*.v "$SSH_ID":/tmp/"$PROJECT_NAME"
-ssh "$SSH_ID" '&&' /home/$HOST_NAME/.local/bin/efx_serv.sh $PROJECT_NAME
+ssh -x "$SSH_ID" /home/$HOST_NAME/hdd/shreeyash/efx_client/efx_serv.sh $PROJECT_NAME
 scp "$SSH_ID":/tmp/$PROJECT_NAME/"$PROJECT_NAME".tar.gz ./
 if [ $? -ne 0 ]; then
 	echo "failed"
